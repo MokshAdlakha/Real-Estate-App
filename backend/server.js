8454c6d16988926d9697a971fc2dd37b5d74c048
Moksh Adlakha <adlakhamoksh@gmail.com>
2026-04-25 14:52:49 +0530

    fixed port for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,4 +10,8 @@ app.use("/leads", require("./routes/leads"));
 app.use("/properties", require("./routes/properties"));
 app.use("/deals", require("./routes/deals"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
